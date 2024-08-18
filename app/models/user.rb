c74dbc79af4122a1f3ae32904f0b15238d56d421
iSaacSigei <kiplangatisack.is@gmail.com>
2024-08-19 06:31:16 +1000

    config

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ApplicationRecord
     has_secure_password
-  
+    has_many :import_orders
+    has_many :export_orders
     # Validations
     validates :email, presence: true, uniqueness: true
     validates :password, presence: true, length: { minimum: 6 }, if: -> { new_record? || !password.nil? }
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,6 @@ Rails.application.routes.draw do
   post '/login', to: 'sessions#create'
   delete '/logout', to: 'sessions#destroy'
   get '/sessions', to: 'sessions#show' # Add this route
-  resources :import_orders, only: [:create]
-  resources :export_orders, only: [:create]
+  resources :export_orders, only: [:create, :index, :show]
+  resources :import_orders, only: [:create, :show, :index]
 end
